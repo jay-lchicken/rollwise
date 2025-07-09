61935465129bb20e50e4f8109dfdc328bf8bfad2
HONGYU-PROMAX <167660133+jay-lchicken@users.noreply.github.com>
2025-07-10 06:52:07 +0800

    Update route.js

--- src/app/api/markAttendance/route.js
+++ src/app/api/markAttendance/route.js
@@ -96,7 +96,7 @@ markResult = await client.query(
   DO UPDATE SET 
     hashed_userid_email = EXCLUDED.hashed_userid_email,
     isattended = true,
-    dateadded = DEFAULT
+    dateadded = DEFAULT,
     image_url = $5
   RETURNING *;
   `,
